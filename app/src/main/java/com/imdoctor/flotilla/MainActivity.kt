85111782659949fbd1760445c6a0987338bc44aa
Andrew 'ImDoctor <andrew@imdoc.ru>
2025-10-26 04:46:17 +0300

    Use game UI

--- app/src/main/java/com/imdoctor/flotilla/MainActivity.kt
+++ app/src/main/java/com/imdoctor/flotilla/MainActivity.kt
@@ -5,43 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.imdoctor.flotilla.ui.theme.FlotillaTheme
+import androidx.navigation.compose.rememberNavController
+import com.imdoctor.flotilla.presentation.navigation.FlotillaNavGraph
+import com.imdoctor.flotilla.presentation.theme.FlotillaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             FlotillaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    val navController = rememberNavController()
+                    FlotillaNavGraph(navController = navController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FlotillaTheme {
-        Greeting("Android")
     }
 }

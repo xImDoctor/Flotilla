af00c600f781f5787bf343c4623c697475e239fe
Andrew 'ImDoctor <andrew@imdoc.ru>
2025-12-26 02:03:34 +0300

    Critical release fix #4
    Fixed client output and server logic, removed unsed application script

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "0.9.4b"
+        versionName = "0.10.0b-mt"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
